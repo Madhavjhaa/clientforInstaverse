21e135829d8986dc496e9e71b45e9dff3b417d19
Madhav jha <91075307+Madhavjhaa@users.noreply.github.com>
2022-08-05 14:08:39 +0530

    Add files via upload

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+import stories from "./stories";
+import authentication from "./authentication";
+
+export default combineReducers({
+    stories,
+    authentication  
+});
+   
